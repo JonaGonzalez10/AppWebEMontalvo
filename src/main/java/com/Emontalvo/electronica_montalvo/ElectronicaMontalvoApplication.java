6261ffa0cc4e8a833be744d800cc114641e2a0ca
JonaGonzalez10 <79065143+JonaGonzalez10@users.noreply.github.com>
2024-07-29 14:25:47 -0600

    Se agrega el proyecto con su estructura y archivos estaticos

---
+++ src/main/java/com/Emontalvo/electronica_montalvo/ElectronicaMontalvoApplication.java
@@ -0,0 +1,13 @@
+package com.Emontalvo.electronica_montalvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicaMontalvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicaMontalvoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Emontalvo/electronica_montalvo/ElectronicaMontalvoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Emontalvo.electronica_montalvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicaMontalvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
